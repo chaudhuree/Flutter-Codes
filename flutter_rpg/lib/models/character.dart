aac81f0d182d09d2669c28f12151459ef89b361a
chaudhuree <chaudhuree00@gmail.com>
2025-07-24 18:15:39 +0600

    feat: add Character and Stats models with stat management functionality

---
+++ flutter_rpg/lib/models/character.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_rpg/models/stats.dart';
+
+class Character with Stats {
+  // constructor
+  Character({required this.name, required this.slogan, required this.id});
+
+  // fields
+  final String name;
+  final String slogan;
+  final String id;
+  bool _isFav = false;
+
+  // methods
+  void toggleIsFav() {
+    _isFav = !_isFav;
+  }
+
+  // getter
+
+  bool get isFav => _isFav;
+}
