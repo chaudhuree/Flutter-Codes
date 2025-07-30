207237741b46dd2f47f55cfd11b56d80f9f81726
chaudhuree <chaudhuree00@gmail.com>
2025-07-30 16:54:36 +0600

    feat: add Character and Vocation models with core RPG attributes and classes

--- flutter_rpg/lib/models/character.dart
+++ flutter_rpg/lib/models/character.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats {
   // constructor
-  Character({required this.name, required this.slogan, required this.id});
+  Character({
+    required this.vocation,
+    required this.name,
+    required this.slogan,
+    required this.id,
+  });
 
   // fields
+  final Vocation vocation;
   final String name;
   final String slogan;
   final String id;
